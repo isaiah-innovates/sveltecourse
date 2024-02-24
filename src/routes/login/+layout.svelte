739404ea1c167b9e3e5076398a564a8c5aeaa88b
isaiah-innovates <128506314+isaiah-innovates@users.noreply.github.com>
2024-02-24 07:24:40 -0500

    added typography

---
+++ src/routes/login/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    
+</script>
+
+<main class="card w-4/6 bg-neutral text-neutral-content mx-auto">
+    <div class="min-h-screen flex flex-col">
+        <slot>
+
+        </slot>
+    </div>
+
+</main>
