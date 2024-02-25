a282a3a15da39b054206c3bd5fd62b45117bb4ee
isaiah-innovates <128506314+isaiah-innovates@users.noreply.github.com>
2024-02-25 07:43:40 -0500

    added google auth

---
+++ src/lib/components/AnimatedRoute.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { fly } from "svelte/transition";
+    import { page } from "$app/stores";
+</script>
+
+{#key $page.url}
+    <div in:fly = {{ x:'-100%', dration: 400 }}>
+        <slot />
+    </div>
+{/key}
