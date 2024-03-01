172db527648250b085bfa37acdd5d5f09dc2bd22
isaiah-innovates <128506314+isaiah-innovates@users.noreply.github.com>
2024-03-01 09:25:27 -0500

    Verify Cookies

--- src/routes/api/signin/+server.ts
+++ src/routes/api/signin/+server.ts
@@ -1,4 +1,4 @@
-import {adminAuth} from '$lib/server/admin';
+import { adminAuth } from '$lib/server/admin';
 import { error, json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
@@ -10,7 +10,7 @@ export const POST: RequestHandler = async ({ request, cookies }) => {
 
     const decodeIdToken = await adminAuth.verifyIdToken(idToken);
 
-    if (new Date().getTime() / 1000 - decodedIdToken.auth_time < 5 * 60) {
+    if (new Date().getTime() / 1000 - decodeIdToken.auth_time < 5 * 60) {
         const cookie = await adminAuth.createSessionCookie(idToken, { expiresIn });
         const options = { maxAge: expiresIn, httpOnly: true, secure: true, path: '/' };
         
